4a106aa11bff6b6976c6359bd21a9a3ad6e81f5c
Simaire <simon.cherrier07@gmail.com>
2022-05-15 12:28:23 +0200

    V 0.0.1

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
+import os
 import discord
+from dotenv import load_dotenv
 
+load_dotenv(dotenv_path="config")
+load_dotenv(dotenv_path="Clef")
+
+TOKEN = os.getenv("TOKEN")
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("Sstem ON")
+
+
+
+client.run(TOKEN)

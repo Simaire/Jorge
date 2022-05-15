a2ca4d8453a47c63ad1b5c1aa6add1d8e1450291
Simaire <simon.cherrier07@gmail.com>
2022-05-15 14:37:48 +0200

    V 0.0.2

--- main.py
+++ main.py
@@ -1,17 +1,56 @@
 import os
 import discord
 from dotenv import load_dotenv
+import openai
 
 load_dotenv(dotenv_path="config")
 load_dotenv(dotenv_path="Clef")
 
 TOKEN = os.getenv("TOKEN")
+openaikey = os.getenv("openaikey")
+
 client = discord.Client()
 
 @client.event
 async def on_ready():
     print("Sstem ON")
 
+@client.event
+async def on_message(message):
 
+    Bot = message.author.bot
+
+    print("Msg Recue")
+    if Bot == False:
+        print("Par un human")
+
+        await message.channel.send(message.content)
+
+        openai.api_key = openaikey
+
+        start_sequence = "\nAI:"
+        restart_sequence = "\nHuman: "
+
+        response = openai.Completion.create(
+            engine="text-davinci-002",
+            prompt="Ce qui suit est une conversation avec un assistant IA. L'assistant est serviable, créatif, intelligent et très sympathique.\n\nHumain : Bonjour, qui êtes-vous ?\nIA : Je suis une IA créée par OpenAI. Comment puis-je vous aider aujourd'hui?\nHuman: " + input(
+                "Human: "),
+            temperature=0.9,
+            max_tokens=150,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0.6,
+            stop=[" Human:", " AI:"]
+        )
+        content = response.choices[0].text
+
+
+        await message.channel.send(content)
+
+    if Bot == True:
+        print("Par un robot")
+
+    if Bot != True and Bot != False:
+        print("Eror")
 
 client.run(TOKEN)
